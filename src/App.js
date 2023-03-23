3a42f18856933c49756ca6ff21753e7353641df9
aamir <aamirp1@outlook.com>
2023-03-23 01:17:03 +0000

    Add dark theme, pension & pg loan

--- src/App.js
+++ src/App.js
@@ -11,21 +11,28 @@ const NI_RATE_ADDITIONAL = 0.02;
 
 const App = () => {
   const [income, setIncome] = useState('');
+  const [percentage, setPercentage] = useState('');
   const [results, setResults] = useState({
     submittedIncome: null,
     incomeTax: null,
     niTax: null,
     studentTaxOne: null,
     studentTaxTwo: null,
+    pgLoan: null,
+    pension: null,
   });
   const [checkedLoans, setCheckedLoans] = useState({
     planOne: false,
     planTwo: false,
+    pgLoan: false,
+    pension: false,
   });
 
   const [submittedCheckedLoans, setSubmittedCheckedLoans] = useState({
     planOne: false,
     planTwo: false,
+    pgLoan: false,
+    pension: false,
   });
 
   const handleChange = (e) => {
@@ -42,6 +49,10 @@ const App = () => {
     const studentTaxTwo = checkedLoans.planTwo
       ? calculatePlanTwoLoan(income)
       : null;
+    const pgLoan = checkedLoans.pgLoan ? calculatePgLoan(income) : null;
+    const pension = checkedLoans.pension
+      ? calculatePension(income, percentage)
+      : null;
 
     setResults({
       submittedIncome: income,
@@ -49,6 +60,8 @@ const App = () => {
       niTax,
       studentTaxOne,
       studentTaxTwo,
+      pgLoan,
+      pension,
     });
     setSubmittedCheckedLoans(checkedLoans);
   };
@@ -57,7 +70,8 @@ const App = () => {
     setCheckedLoans({ ...checkedLoans, [plan]: !checkedLoans[plan] });
   };
 
-  function calculateIncomeTax(income) {
+  function calculateIncomeTax(salary) {
+    let income = salary - calculatePension(salary, percentage);
     const personalAllowance =
       income <= 100000
         ? PERSONAL_ALLOWANCE
@@ -126,12 +140,30 @@ const App = () => {
     }
     return tax.toFixed(2);
   }
+
+  function calculatePgLoan(income) {
+    let tax = 0;
+    if (income > 21000) {
+      const taxableIncome = income - 21000;
+      tax = taxableIncome * 0.06;
+    }
+    return tax.toFixed(2);
+  }
+
+  function calculatePension(income, percentage) {
+    console.log(income, percentage);
+    let pension = income * (percentage / 100);
+    return pension;
+  }
+
   const netIncome = (
     results.submittedIncome -
     results.incomeTax -
     results.niTax -
     (submittedCheckedLoans.planOne ? results.studentTaxOne : 0) -
-    (submittedCheckedLoans.planTwo ? results.studentTaxTwo : 0)
+    (submittedCheckedLoans.planTwo ? results.studentTaxTwo : 0) -
+    (submittedCheckedLoans.pgLoan ? results.pgLoan : 0) -
+    (submittedCheckedLoans.pension ? results.pension : 0)
   ).toFixed(2);
 
   return (
@@ -173,28 +205,76 @@ const App = () => {
               className='checkbox'
             />
           </label>
+          <label className='label cursor-pointer'>
+            <span className='label-text'>Postgraduate student loan</span>
+            <input
+              type='checkbox'
+              checked={checkedLoans.pgLoan}
+              onChange={() => handleCheckboxChange('pgLoan')}
+              className='checkbox'
+            />
+          </label>
+          <label className='label cursor-pointer'>
+            <span className='label-text'>Pension</span>
+            {checkedLoans.pension ? (
+              <input
+                min='0.01'
+                step={0.01}
+                type='number'
+                name='pension'
+                value={percentage}
+                placeholder='£'
+                className='input input-bordered w-full max-w-xs'
+                onChange={(e) => setPercentage(e.target.value)}
+                onWheel={(e) => {
+                  e.preventDefault();
+                }}
+              />
+            ) : (
+              ''
+            )}
+            <input
+              type='checkbox'
+              checked={checkedLoans.pension}
+              onChange={() => handleCheckboxChange('pension')}
+              className='checkbox'
+            />
+          </label>
+
           <button type='submit' className='btn'>
             Calculate!
           </button>
-          <div>
-            {results.submittedIncome !== null && (
-              <div>
-                Your net income is: £{netIncome}
-                <br />
-                Income Tax: £{results.incomeTax} <br />
-                NI Tax: £{results.niTax} <br />
-                {submittedCheckedLoans.planOne && results.studentTaxOne && (
-                  <>
-                    Student loan 1: £{results.studentTaxOne} <br />{' '}
-                  </>
-                )}
-                {submittedCheckedLoans.planTwo && results.studentTaxTwo && (
-                  <>Student loan 2: £{results.studentTaxTwo}</>
-                )}
-              </div>
-            )}
-          </div>
         </form>
+
+        <div>
+          {results.submittedIncome !== null && (
+            <div>
+              Your net income is: £{netIncome}
+              <br />
+              Income Tax: £{results.incomeTax} <br />
+              NI Tax: £{results.niTax} <br />
+              {submittedCheckedLoans.planOne && results.studentTaxOne && (
+                <>
+                  Student loan 1: £{results.studentTaxOne} <br />{' '}
+                </>
+              )}
+              {submittedCheckedLoans.planTwo && results.studentTaxTwo && (
+                <>
+                  Student loan 2: £{results.studentTaxTwo}
+                  <br />
+                </>
+              )}
+              {submittedCheckedLoans.pgLoan && results.pgLoan && (
+                <>
+                  Postgraduate loan: £{results.pgLoan} <br />
+                </>
+              )}
+              {submittedCheckedLoans.pension && results.pension && (
+                <>Pension: £{results.pension}</>
+              )}
+            </div>
+          )}
+        </div>
       </div>{' '}
     </div>
   );
